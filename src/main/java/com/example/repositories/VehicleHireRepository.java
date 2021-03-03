539af8e7464a9ba8c0365eec0b1cd7a49838a73d
Mahadi <you@example.com>
2021-03-03 20:03:44 +0600

    Some frontend stafs added

--- src/main/java/com/example/repositories/VehicleHireRepository.java
+++ src/main/java/com/example/repositories/VehicleHireRepository.java
@@ -1,12 +1,12 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.model.VehicleHire;
-
-
-@Repository
-public interface VehicleHireRepository extends JpaRepository<VehicleHire, Integer> {
-
-}
+//package com.example.repositories;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.example.model.VehicleHire;
+//
+//
+//@Repository
+//public interface VehicleHireRepository extends JpaRepository<VehicleHire, Integer> {
+//
+//}
